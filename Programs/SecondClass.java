1af15c10d64ba501def482dbd0fc1f443c769967
Hari Vardhan Reddy Peddireddy <Harivardhanreddy.Peddireddy@blueyonder.com>
2024-06-03 08:31:15 +0530

    03-June-2024

---
+++ Programs/SecondClass.java
@@ -0,0 +1,14 @@
+package Programs;
+
+public class SecondClass {
+    public static void main(String[] args) {
+        System.out.println("Peddireddy");
+        int array[] = {2, 3, 6, 9, 7, 5};
+        int Largest = array[0];
+        for (int i = 0; i < array.length; i++) {
+            System.out.println(array[i]);
+
+        }
+    }
+}
+
